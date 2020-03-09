7ee9321ae9164a100932fe9726af04dfe98c726a
Roman Torres <rtorres@envivent.com>
2020-03-09 11:23:46 -0700

    functioning and locked scroll

--- js/main.js
+++ js/main.js
@@ -3,31 +3,45 @@ class PopUp{
         this.el = document.querySelector(".popuppage");
         this.link = this.el.querySelectorAll(".popup");
         this.overlay = this.el.querySelector(".overlay");
-        this.popupwindow = this.el.querySelector(".popupwindow");
-        this.close = this.el.querySelectorAll(".closers");
+        this.x = this.el.querySelectorAll(".closers");
 
-
+        // Event Listeners for the links to open popup
         for (let i = 0; i < this.link.length; i++) {
-            this.link[i].addEventListener("click", () => {
-                this.overlay.classList.add("active");
-                this.popupwindow = this.el.querySelector(".popupwindow").classList.add("active");
-        });
+                this.open(this.link[i]);
+        };
+
+        // Event Listeners for the X icon to close popup
+        for (let i = 0; i < this.x.length; i++) {
+            this.x[i].addEventListener("click", () => {
+                this.el.querySelector(".overlay").classList.remove("active");
+                this.el.querySelector(".active").classList.remove("active");
+                document.querySelector("body").classList.remove("popupactive");
+            });
         }
 
-        function checkKeyPress(key) {  
-            if (key.keyCode == "27") {
-                document.querySelector(".overlay").classList.remove("active");
-                document.querySelector(".popupwindow").classList.remove("active");
-            }
-        }
+        // Closes popups on ESC keydown
+        this.el.addEventListener("keydown", this.checkKeyPress, false);
+    }
 
-        window.addEventListener("keydown", checkKeyPress, false);
-                
-        for (let i = 0; i < this.close.length; i++) {
-            this.close[i].addEventListener("click", () => {
-                document.querySelector(".overlay").classList.remove("active");
-                document.querySelector(".popupwindow").classList.remove("active");
+    open(sel) {
+        sel.addEventListener("click", () => {
+            this.overlay.classList.add("active");
+            this.el.querySelector("." + sel.id).classList.add("active");
+            //locks scroll
+            document.querySelector("body").classList.add("popupactive");
         });
+    }
+
+    close(sel) {
+
+    }
+
+    // Function for ESC keydown event to close popup
+    checkKeyPress(key) {  
+        if (key.keyCode == "27") {
+            document.querySelector(".overlay").classList.remove("active");
+            document.querySelector("body").classList.remove("popupactive");
+            document.querySelector(".active").classList.remove("active");
         }
     }
 }
